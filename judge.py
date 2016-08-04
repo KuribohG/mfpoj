ef1270400a27abd5645e3de2cd4c8d4482400d01
xuruifan <707185547@qq.com>
2016-08-04 16:06:52 +0800

    judge_C++

--- judge.py
+++ judge.py
@@ -8,10 +8,21 @@ django.setup()
 
 from oj.models import Waiting
 
-while True:
-    waiting_list = Waiting.objects.all()
-    if waiting_list:
-        for waiting in waiting_list:
-            waiting.delete()
-    else:
-        time.sleep(1)
+waiting_list = Waiting.objects.all()
+if waiting_list:
+    for waiting in waiting_list:
+        waiting_language=waiting.submission.language
+        waiting_source=waiting.submission.source
+        #print(waiting_language)
+        if  waiting_language == "C++" :
+            #print("g++")
+            #print(waiting_source)
+            waiting_output=open("/tmp/test.cpp",'w')
+            waiting_output.write(waiting_source)
+            waiting_output.close()
+            os.system("g++ -o /tmp/test /tmp/test.cpp")
+            os.system("/tmp/test")
+        elif waiting_language == "Python" :
+            #print("Python3")
+            pass
+        waiting.delete()
