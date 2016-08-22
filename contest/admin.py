61c9b6b8bf826e88e34e9b57af177c128b30a535
Yuheng Zou <zouyuheng1998@gmail.com>
2016-08-22 15:55:30 +0800

    modify model doc str

--- contest/admin.py
+++ contest/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Contest, ContestProblem, ContestSubmission
+from .models import Contest, ContestProblem, ContestSubmission, ContestUser
 
 admin.site.register(Contest)
 admin.site.register(ContestProblem)
+admin.site.register(ContestUser)
 admin.site.register(ContestSubmission)
--- contest/models.py
+++ contest/models.py
@@ -1,16 +1,26 @@
 from django.db import models
 
-from oj.models import Problem, Submission
+from oj.models import Problem, Submission, User
 
 class Contest(models.Model):
     title = models.CharField(max_length=100)
     
     def __str__(self):
         return self.title
+
+class ContestUser(models.Model):
+    contest = models.ForeignKey(Contest, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.contest.title + self.user.username
     
 class ContestProblem(models.Model):
     contest = models.ForeignKey(Contest, on_delete=models.CASCADE)
     problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.contest.title + self.problem.title
 
 class ContestSubmission(models.Model):
     contest = models.ForeignKey(Contest, on_delete=models.CASCADE)
