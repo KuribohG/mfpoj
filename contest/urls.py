e24f70bc9ced34f4e2a8791560615d42acdbc24d
zidaneandmessi <dimar@qq.com>
2016-08-24 16:54:51 +0800

    add contest problem page and contest status page

--- contest/urls.py
+++ contest/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^contests/$', views.contests, name='contests'),
     url(r'^contest/(?P<contest_id>[0-9]+)/$', views.contest, name='contest'),  
     url(r'^contest/(?P<contest_id>[0-9]+)/submit/(?P<problem_id>[A-Z]+)/$', views.contest_submit, name='contest_submit'), 
-    url(r'^contest/(?P<contest_id>[0-9]+)/submit/$', views.contest_submit, name='contest_submit'), 
+    url(r'^contest/(?P<contest_id>[0-9]+)/problem/(?P<problem_id>[A-Z]+)/$', views.contest_problem, name='contest_problem'), 
+    url(r'^contest/(?P<contest_id>[0-9]+)/status/$', views.contest_status, name='contest_status'),
 ]
--- contest/views.py
+++ contest/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
 from .models import Contest, ContestProblem, ContestSubmission, ContestUser
-from oj.models import User, Waiting, Submission
+from oj.models import User, Waiting, Submission, Problem
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 def paginate(object_list, objects_per_page, now_page):
@@ -95,3 +95,56 @@ def contest(request, contest_id):
     }
 
     return render(request, 'contest.html', context)
+    
+def contest_status(request, contest_id):
+    contest = Contest.objects.get(pk=contest_id)
+    submission_list = contest.contestsubmission_set.all()
+    submission_list = list(submission_list)
+    submission_list.reverse()
+    
+    #分页大法开启
+    submissions_per_page = 10
+    paginator = Paginator(submission_list, submissions_per_page)
+    if 'page' in request.GET.keys():
+        nowpage = request.GET['page']
+    else:
+        nowpage = 1
+    try:
+        submission_list = paginator.page(nowpage) 
+    except PageNotAnInteger:
+        submission_list = paginator.page(1)
+    except EmptyPage:
+        submission_list = paginator.page(paginator.num_pages)
+    #分页大法结束
+    
+    logined = 'username' in request.session.keys()
+    
+    context = {
+        'contest_id': contest_id, 
+        'submission_list': submission_list, 
+        'logined': int(logined),  
+        'name': request.session['username'] if logined else '',
+    }
+    return render(request, 'contest_status.html',context)
+
+def contest_problem(request, **kwargs):
+    logined = 'username' in request.session.keys()
+    contest_id = int(kwargs['contest_id'])
+    problem_id_letter = kwargs.get('problem_id')
+    if problem_id_letter is None:
+        problem_id_letter = 'A'
+    problem_id = ord(problem_id_letter) - ord('A')
+    contest = Contest.objects.get(pk=contest_id)
+    contest_problem = contest.contestproblem_set.all()[problem_id]
+    real_id = contest_problem.problem.id
+    problem = Problem.objects.get(pk=real_id)
+
+    context = {
+        'problem_id_letter':problem_id_letter,
+        'contest_id': contest_id, 
+        'problem': problem,
+        'logined': int(logined),  
+        'name': request.session['username'] if logined else '',
+    }
+    return render(request, 'contest_problem.html', context)
+    
