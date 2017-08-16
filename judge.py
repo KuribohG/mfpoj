78ee5390d9a7a7af6d7b2f2c6dc1e0e8b50d23b8
root <jcysndn@163.com>
2017-08-16 09:53:29 +0800

    added compile time limit

--- judge.py
+++ judge.py
@@ -24,7 +24,7 @@ RESULT_STR = [
 	'System Error', 
 ]
 
-def judge(waiting):
+def compile_cpp(waiting):
 	submission = waiting.submission
 	language = submission.language
 	source = submission.source
@@ -33,11 +33,11 @@ def judge(waiting):
 		output = open("/tmp/test.cpp", 'w')
 		output.write(source)
 		output.close()
-		judge_CE = os.system("g++ -o /tmp/test /tmp/test.cpp")
-		if judge_CE == 0:
-			return "/tmp/test"
+		os.system("rm -rf /tmp/test;ulimit -t 5;g++ -o /tmp/test /tmp/test.cpp;ulimit -t unlimited");
+		if os.path.exists("/tmp/test"):
+			return True
 		else:
-			return "Compile Error"
+			return False
 
 def solve_output(f):
 	with open(f,'r') as fo:
@@ -67,29 +67,22 @@ def get_status_from_result(result):
 	return max([obj['result'] for obj in result])
 
 def run_one_testcase(testcase):
-	os.popen("echo "+testcase.input.file.name)
-	#command = exec_file
 	os.system("cp "+testcase.input.file.name+" /tmp/input.in")
-	print(testcase.time_limit)
-	res=os.popen("cp /usr/bin/time /tmp/time;docker run -v /tmp:/mnt -m%dM ubuntu:17.04 /bin/bash -c \"cd /mnt;ulimit -t %d; date +\'startoifajdsnvcmewrlk %%s%%N\'; { ./time -f \'memoryeiurojlkfdsjorewlkmfnaowrtoinalkdsf %%M\' ./test <input.in >output.out; } 2>&1; date +\'endnvajdfsdoifamfeie %%s%%N\'\""%((testcase.memory_limit+100)*1024,testcase.time_limit+1)).readlines() 
-	print(res)
-	# os.system(exec_file + " < " + testcase.input.file.name + " > /tmp/test.out"
+	res=os.popen("cp /usr/bin/time /tmp/time;docker run -v /tmp:/mnt -m%dM ubuntu /bin/bash -c \"cd /mnt;ulimit -t %d; date +\'startoifajdsnvcmewrlk %%s%%N\'; { ./time -f \'memoryeiurojlkfdsjorewlkmfnaowrtoinalkdsf %%M\' ./test <input.in >output.out; } 2>&1; date +\'endnvajdfsdoifamfeie %%s%%N\'\""%((testcase.memory_limit+100)*1024,testcase.time_limit+1)).readlines() 
 	rst={
 		'result':'','timeused':-1,'memoryused':-1
 	}
-	stt=0
-	edt=-1
-	print (len(res))
+	starttime=0
+	endtime=-1
 	for lin in res:
 		lst=lin.split()
-		print (lst)
 		if lst[0]=='memoryeiurojlkfdsjorewlkmfnaowrtoinalkdsf':
 			rst['memoryused']=int(lst[1])
 		if lst[0]=='startoifajdsnvcmewrlk':
-			stt=int(lst[1])
+			starttime=int(lst[1])
 		if lst[0]=='endnvajdfsdoifamfeie':
-			edt=int(lst[1])
-			rst['timeused']=(edt-stt)/1000000
+			endtime=int(lst[1])
+			rst['timeused']=(endtime-starttime)/1000000
 	if rst['memoryused']>testcase.memory_limit*1024:
 		rst['result']='Memory Limit Exceeded'
 	elif rst['timeused']>testcase.time_limit*1000:
@@ -103,16 +96,17 @@ def run_one_testcase(testcase):
 			rst['result']='Wrong Answer'
 	return rst
 
-def run_testcases(waiting, exec_file):
+def run_testcases(waiting):
 	submission = waiting.submission
 	language = submission.language
 
 	result = []
+	command = ""
 	
 	if language == "C++":
 		testcases = len(submission.problem.testcase_set.all())
 		ac = 0
-		for testcase in submission.problem.testcase_set.all():
+		for testcase in submission.problem.testcase_set.all():	
 			rst = run_one_testcase(testcase)
 			if rst['result'] == 'Accepted' :
 				ac += 1
@@ -189,13 +183,9 @@ while True:
 	waiting_list = Waiting.objects.all()
 	if waiting_list:
 		for waiting in waiting_list:
-		
 			s = waiting.submission.user
-			#s.waiting -= 1
-			#s.save()
-			exec_file = judge(waiting)
-			if exec_file != "Compile Error":
-				run_testcases(waiting, exec_file)
+			if compile_cpp(waiting):
+				run_testcases(waiting)
 			else:
 				solve_CE()
 				obj = json.loads(s.stat)
